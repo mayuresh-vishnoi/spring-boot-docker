d9a9606635cc206775937e5b041c4c1eab4e2e70
Mayuresh <adad54040@gmail.com>
2026-08-10 04:17:23 +0530

    added ! message

--- src/main/java/com/mayur29/spring_boot_app/controller/WelcomeController.java
+++ src/main/java/com/mayur29/spring_boot_app/controller/WelcomeController.java
@@ -11,6 +11,6 @@ public class WelcomeController {
 
     @GetMapping
     public ResponseEntity<String> welcome(){
-        return ResponseEntity.ok("Welcome to spring boot app!!");
+        return ResponseEntity.ok("Welcome to spring boot app!");
     }
 }
